e662925e9141872d1032a0d94f437b6709bff34c
a-wart <wart@Arts-MacBook-Air.local>
2016-05-14 23:38:42 +0700

    params fix

--- app/notify_bot.js
+++ app/notify_bot.js
@@ -6,8 +6,8 @@ function *notifyBot() {
   let notification = this.request.body;
 
   let mid = notification.id;
-  let departDate = notification.departDate;
-  let returnDate = notification.returnDate;
+  let departDate = notification.depart_date;
+  let returnDate = notification.return_date;
   let title = notification.title;
   let text = `We've found a ticket! ${title}. Depart date is ${departDate}; return date is ${returnDate}`;
 
