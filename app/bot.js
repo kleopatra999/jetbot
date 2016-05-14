9dbafd4b1ec62d994a6a224a5d5a428888dd4561
a-wart <wart@Arts-MacBook-Air.local>
2016-05-14 17:00:42 +0700

    tiny refactoring

--- app/bot.js
+++ app/bot.js
@@ -1,8 +1,6 @@
 "use strict";
+const LINE_CHANNEL_TOKEN = '6BlHqpsKmwNPlxSbppx1bxDVIvqmJD3wGk8/+XYAs5gSaTaUivxeImbm+37rKU1qNgwg8FD09QsYIjbHc6Tvqptn186izvhKQqqBMMFOaB+xMikqtRnU5ds9zpq3k2ZpyY89kiBglxxZl0qvUrdyX618BSl7lGXPAT9HRw/DX2c=';
 const request = require('request-promise');
-
-const CHANNEL_SECRET = '9d9ca5008c9ce541e3b575112633c97d';
-const CHANNEL_ID = '1466971612';
 
 function *bot(next) {
   this.body = {
@@ -10,20 +8,17 @@ function *bot(next) {
   };
 
   let result = this.request.body.result[0];
-  let fromId = result.from;
   let userMid = result.content.from;
-
 
   console.log(result.content.text)
   let suggest = yield* getSuggest(result.content.text);
 
   console.log(suggest)
-  console.log(suggest[0].title);
+  console.log(suggest[0]);
 
-  yield* send('Hello, world', {
-    channelMid: fromId,
+  yield* send({
     userMid: userMid,
-    text: suggest[0] && suggest[0].title || 'Sorry mista, place not found'
+    text: suggest[0] && suggest[0].title || 'Sorry we haven\'t, found the place:('
   });
 }
 
@@ -36,12 +31,12 @@ function *getSuggest(query) {
   });
 }
 
-function *send(message, params) {
+function *send(params) {
   const options = {
     method: 'POST',
     url: 'https://api.line.me/v1/events',
     headers: {
-      'X-LINE-ChannelToken': '6BlHqpsKmwNPlxSbppx1bxDVIvqmJD3wGk8/+XYAs5gSaTaUivxeImbm+37rKU1qNgwg8FD09QsYIjbHc6Tvqptn186izvhKQqqBMMFOaB+xMikqtRnU5ds9zpq3k2ZpyY89kiBglxxZl0qvUrdyX618BSl7lGXPAT9HRw/DX2c='
+      'X-LINE-ChannelToken': LINE_CHANNEL_TOKEN
     },
     body: {
       "to": [ params.userMid ],
