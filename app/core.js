e2f02629d29dbeb1b425903b5cc36bd7f673a364
Anton Medvedev <anton@medv.io>
2016-05-14 20:58:31 +0700

    :gem:

--- app/core.js
+++ app/core.js
@@ -22,6 +22,12 @@ function *core(request) {
 
   console.log('REQUEST'.yellow, text);
 
+  // Split text to words.
+  let words = text
+    .toLowerCase()
+    .replace(/[^\w]/ig, ' ')
+    .split(/\s+/);
+
   if (!store.hasOwnProperty(mid)) {
     context = store[mid] = {
       originName: '',
@@ -37,33 +43,47 @@ function *core(request) {
     context = store[mid];
   }
 
-  if(!context.originName) {
+  if (!context.originName) {
     let suggest = yield* getSuggest(text);
     context.originName = suggest[0] && suggest[0].title;
     context.originIata = suggest[0] && suggest[0].code;
 
-    yield* sendMessage({mid, text: `Okay, flight from ${context.originName}`});
+    yield* sendMessage({mid, text: `Okay, flight from ${context.originName}.`});
     yield* sendMessage({mid, text: `Where are you going to?`});
-  } else if(!context.destinationName) {
+  } else if (!context.destinationName) {
     let suggest = yield* getSuggest(text);
     context.destinationName = suggest[0] && suggest[0].title;
     context.destinationIata = suggest[0] && suggest[0].code;
 
-    yield* sendMessage({mid, text: `Okay, flight to ${context.destinationName}`});
+    yield* sendMessage({mid, text: `Okay, flight to ${context.destinationName}.`});
     yield* sendMessage({mid, text: `When?`});
-  } else if(context.months) {
-    context.months = [1,2,3];
+  } else if (context.months) {
+    const months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+    const monthsShort = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+
+    words.forEach((word) => {
+      let month = months.indexOf(word);
+      if(month != -1) {
+        context.months.push(month);
+      } else {
+        let month = monthsShort.indexOf(word);
+        if (month != -1) {
+          context.months.push(month);
+        }
+      }
+    });
+
+    yield* sendMessage({mid, text: `Okay, i will search flights in ${context.months.map(i => months[i]).join(', ')}.`});
   }
 
   if (isFilled(context)) {
-    //
     delete store[mid];
     yield* sendMessage({mid, text: `Okay, i say you when cheap price`});
   }
 }
 
 function isFilled(context) {
-  return context.originName && context.destinationName && context.months;
+  return context.originName && context.destinationName && context.months.length;
 }
 
 module.exports = core;
