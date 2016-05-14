df912f28931d06fc37b7fb98228f6e89e4e20ba1
jr-korakot <k.khotcharit@aviasales.ru>
2016-05-14 23:15:02 +0700

    remove contentMetadata log

--- app/send_message.js
+++ app/send_message.js
@@ -122,7 +122,6 @@ send.rich = function *rich(params) {
   };
 
   console.log('OPTIONS', options);
-  console.log('contentMetadata', options.body.content.contentMetadata);
 
   yield request(options);
 }
