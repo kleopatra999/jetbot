73b5753ffcf7a415c70581d3acd3262d634d8fe5
Anton Medvedev <anton@medv.io>
2016-05-15 11:08:27 +0700

    :gem:

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 require('colors');
 const koa = require('koa');
 const body = require('koa-body');
+const serve = require('koa-static');
 const router = require('./app/router');
 const logger = require('./app/middlewares/logger');
 
 const app = koa();
 app.use(logger);
 app.use(body());
+app.use(serve(__dirname + '/public'));
 app.use(router.routes());
 
 const port = process.env.PORT || 3000;
